1b2e8277f0c56f098608056a9de80b4a43adb467
johnnymat <turchetto@live.it>
2018-12-08 10:31:47 +0100

    Update adovms-7.sh

--- CentOS-7/adovms-7.sh
+++ CentOS-7/adovms-7.sh
@@ -14,8 +14,8 @@ ROUNDCUBE="1.3.8"
 ## REMOTION ??? REPO_GF="http://mirror.ghettoforge.org/distributions/gf/gf-release-latest.gf.el7.noarch.rpm"
 
 # Extra packages
-MAIL_PACKAGES="postfix3 postfix3-cdb postfix3-mysql postfix3-pcre postfix3-perl-scripts postfix3-sqlite dovecot22 dovecot22-mysql dovecot22-pigeonhole clamav-filesystem clamav-server clamav-update clamav-milter-systemd clamav-data clamav-server-systemd clamav-scanner-systemd clamav clamav-milter clamav-lib clamav-scanner"
-EXTRA_PACKAGES="opendkim git subversion libicu  clamav clamav-scanner-systemd"
+MAIL_PACKAGES="postfix3 postfix3-cdb postfix3-mysql postfix3-pcre postfix3-perl-scripts postfix3-sqlite dovecot22 dovecot22-mysql dovecot22-pigeonhole"
+EXTRA_PACKAGES="opendkim git subversion libicu clamav-filesystem clamav-server clamav-update clamav-milter-systemd clamav-data clamav-server-systemd clamav-scanner-systemd clamav clamav-milter clamav-lib clamav-scanner"
 
 # PEAR packages
 PEAR="Net_IDNA2 Mail_mime Mail_mimeDecode Net_LDAP3 Auth_SASL Net_SMTP"
